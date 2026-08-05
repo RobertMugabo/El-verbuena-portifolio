69d1d07769f18d7f583ae9ce812db9a2e3392964
kwizerisezerano <tabitakwizerisezerano@gmail.com>
2026-08-05 18:26:57 +1400

    fx:slowness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -54,10 +54,6 @@ export default function Navbar({ locale, translations }: NavbarProps) {
         <div className="ev-header-controls">
           <NavbarClient locale={locale} onMenuToggle={setMenuOpen} />
         </div>
-
-        <button className="ev-hamburger" onClick={() => setMenuOpen(!menuOpen)} aria-label="Toggle menu">
-          <span /><span /><span />
-        </button>
       </div>
       {menuOpen && <div className="ev-nav-overlay" onClick={() => setMenuOpen(false)} />}
     </header>
